997f48de35abd64a7e0711bea843b0d2c518b9f8
Ulyana2004@mail.ru <Ulyana2004@mail.ru>
2025-10-20 17:52:42 +0300

    docs:  README.md with project description

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lab 1 - Basic UI',
+      title: 'Lab 1 - Basic UI!!',
       theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
       home: const MyHomePage(),
     );
