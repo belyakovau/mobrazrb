dd25e1180886517f2e405fdfba7ff587ccd15d58
Ulyana2004@mail.ru <Ulyana2004@mail.ru>
2025-11-25 13:28:00 +0300

    auth

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,6 +1,4 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AuthScreen extends StatefulWidget {
